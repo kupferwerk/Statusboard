7a3e41517cb1cb275641284c405ea79d04fea835
Mathias Nagler <mathias.nagler@me.com>
2015-12-04 19:48:04 +0100

    Rename ViewController to ProfileViewController

---
+++ Statusboard iOS/ProfileViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Statusboard iOS
+//
+//  Created by Mathias Nagler on 04.12.15.
+//  Copyright © 2015 Kupferwerk GmbH. All rights reserved.
+//
+
+import UIKit
+
+internal class ProfileViewController: UIViewController {
+
+
+
+}
+
