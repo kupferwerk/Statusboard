8cf2d3abe508a0f9219a3f6cb434028d4b59acba
Mathias Nagler <mathias.nagler@me.com>
2015-12-04 19:02:10 +0100

    Adds EmployeeCollectionViewCell

---
+++ Statusboard Shared/EmployeeCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  EmployeeCollectionViewCell.swift
+//  Statusboard
+//
+//  Created by Mathias Nagler on 04.12.15.
+//  Copyright © 2015 Kupferwerk GmbH. All rights reserved.
+//
+
+import UIKit
+
+public class EmployeeCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var avatarImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+}
