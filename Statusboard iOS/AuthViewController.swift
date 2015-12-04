ea42e20f83ad051c74a379a656f5f1e864859a01
Bernhard Loibl <bernhard.loibl@gmail.com>
2015-12-04 20:38:44 +0100

    Adds login ui.

---
+++ Statusboard iOS/AuthViewController.swift
@@ -0,0 +1,13 @@
+//
+//  AuthViewController.swift
+//  Statusboard
+//
+//  Created by Bernhard Loibl on 04/12/15.
+//  Copyright © 2015 Kupferwerk GmbH. All rights reserved.
+//
+
+import UIKit
+
+class AuthViewController: UIViewController {
+
+}
---
+++ Statusboard iOS/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LoginViewController.swift
+//  Statusboard
+//
+//  Created by Bernhard Loibl on 04/12/15.
+//  Copyright © 2015 Kupferwerk GmbH. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    
+    @IBAction func login(sender: UIButton) {
+        
+    }
+}
