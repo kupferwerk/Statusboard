90667df693ec0acc72c183384185b36dbcb841e0
Mathias Nagler <mathias.nagler@me.com>
2015-12-05 04:38:52 +0100

    Adjust AuthViewController and Storyboard

--- Statusboard iOS/AuthViewController.swift
+++ Statusboard iOS/AuthViewController.swift
@@ -14,20 +14,29 @@ import Locksmith
 
 class AuthViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var createButton: UIButton!
     
-    @IBOutlet weak var firstnameTextField: UITextField!
-    @IBOutlet weak var lastnameTextField: UITextField!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var departmentLabel: UILabel!
     @IBOutlet weak var departmentTextField: UITextField!
-    @IBOutlet weak var officeTextField: UITextField!
     
     let firebaseService = FirebaseService()
     var disposeBag = DisposeBag()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        segmentedControl.selectedSegmentIndex = 0
+
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillShow:", name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillHide:", name: UIKeyboardWillHideNotification, object: nil)
         
         let observable = combineLatest(emailTextField.rx_text, passwordTextField.rx_text) { ($0, $1) }
             .sampleLatest(createButton.rx_tap)
@@ -48,21 +57,51 @@ class AuthViewController: UIViewController {
             })
         
         combineLatest(observable,
-            firstnameTextField.rx_text,
-            lastnameTextField.rx_text,
-            departmentTextField.rx_text,
-            officeTextField.rx_text
-        ) { ($0, $1, $2, $3, $4) }
-            .subscribeNext { (authData: FAuthData, firstname, lastname, department, office) -> Void in
+            nameTextField.rx_text,
+            departmentTextField.rx_text
+        ) { ($0, $1, $2) }
+            .subscribeNext { (authData: FAuthData, name, department) -> Void in
                 let user: Dictionary<String, String> = [
-                    "firstname": firstname,
-                    "lastname": lastname,
+                    "name": name,
                     "department": department,
-                    "office": office
                 ]
                 
                 self.firebaseService.updateUser(authData.uid, user: user)
             }
             .addDisposableTo(disposeBag)
     }
+    
+    @IBAction func segmentedControlChanged(sender: AnyObject) {
+        switch segmentedControl.selectedSegmentIndex {
+        case 0:
+            UIView.animateWithDuration(0.2) {
+                self.nameLabel.hidden = false
+                self.nameTextField.hidden = false
+                self.departmentLabel.hidden = false
+                self.departmentTextField.hidden = false
+            }
+        case 1:
+            UIView.animateWithDuration(0.2) {
+                self.nameLabel.hidden = true
+                self.nameTextField.hidden = true
+                self.departmentLabel.hidden = true
+                self.departmentTextField.hidden = true
+            }
+        default: break
+        }
+    }
+    
+    func keyboardWillShow(notification: NSNotification) {
+        let keyboardSize = notification.userInfo![UIKeyboardFrameBeginUserInfoKey]!.CGRectValue.size
+        self.scrollView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardSize.height, right: 0)
+        self.scrollView.scrollIndicatorInsets = UIEdgeInsets(top: 0, left: 0, bottom: keyboardSize.height, right: 0)
+        
+    }
+    
+    func keyboardWillHide(notification: NSNotification) {
+        self.scrollView.contentInset = UIEdgeInsetsZero
+        self.scrollView.scrollIndicatorInsets = UIEdgeInsetsZero
+    }
+    
+
 }
