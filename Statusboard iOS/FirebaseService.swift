cc5a4ec6ddcc86f6d919149e92a0134784236373
Michael Kao <micha.kao@gmail.com>
2015-12-05 01:02:43 +0100

    Add auth sequence

--- Statusboard iOS/FirebaseService.swift
+++ Statusboard iOS/FirebaseService.swift
@@ -13,14 +13,14 @@ import RxSwift
 class FirebaseService {
     let ref = Firebase(url:"https://shining-heat-4070.firebaseio.com")
 
-    func createUserObservable(email: String, password: String) -> Observable<String> {
+    func createUserObservable(email: String, password: String) -> Observable<(String, String, String)> {
         return create { observer in
             self.ref.createUser(email, password: password, withValueCompletionBlock: { error, userData in
                 if error != nil {
                     observer.on(.Error(error))
                 } else {
                     if let uid = userData["uid"] as? String {
-                        observer.on(.Next(uid))
+                        observer.on(.Next((uid, email, password)))
                         observer.on(.Completed)
                     } else {
                         observer.on(.Error(NSError(domain: "create.user", code: 0, userInfo: nil)))
@@ -32,42 +32,24 @@ class FirebaseService {
         }
     }
 
-//    func authUser(email: String, password: String) -> Observable<> {
-//        return create { observer in
-//            self.ref.authUser(email, password: password) { error, authData in
-//                if error != nil {
-//                    observer.on(.Error(error))
-//                } else {
-//                    if let uid = userData["uid"] as? String {
-//                        observer.on(.Next(uid))
-//                        observer.on(.Completed)
-//                    } else {
-//                        observer.on(.Error(NSError(domain: "create.user", code: 0, userInfo: nil)))
-//                    }
-//                }
-//            }
-//            
-//            return NopDisposable.instance
-//        }
-//
-//        ref.authUser(emailTextField.text, password: passwordTextField.text) { error, authData in
-//            if error != nil {
-//                
-//            }
-//            else {
-//                print(authData.token)
-//                print(authData.uid)
-//                
-//                guard let email = authData.providerData["email"] as? String else { return }
-//                
-//                let newUser = [
-//                    "email": email
-//                ]
-//                
-//                self.ref.childByAppendingPath("users")
-//                    .childByAppendingPath(authData.uid).setValue(newUser)
-//            }
-//        }
-//    }
-
+    func authUser(email: String, password: String) -> Observable<FAuthData> {
+        return create { observer in
+            self.ref.authUser(email, password: password) { error, authData in
+                if error != nil {
+                    observer.on(.Error(error))
+                } else {
+                    observer.on(.Next(authData))
+                    observer.on(.Completed)
+                }
+            }
+            
+            return NopDisposable.instance
+        }
+    }
+    
+    func updateUser(uid: String, user: Dictionary<String, String>) {
+        self.ref.childByAppendingPath("users")
+            .childByAppendingPath(uid).setValue(user)
+    }
+    
 }
